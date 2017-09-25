177dc1d308a4e0cb1949256519c3d568f434a976
mashaelym <ma735@njit.edu>
2017-09-25 12:17:12 -0400

    adding a title for strings

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
-$prog =new main();
+$obj =new main();
+$text = "String Functions";
+$obj->StringFunc($text);
+
 class main {
 public function __construct() {
 echo 'This is an object </br>';
 }
-
-
+public function StringFunc(){
+echo '<h1>String Functions:</h1>';
+print($text);
+echo '<hr>';
+}
 
 
 public function __destruct() {
