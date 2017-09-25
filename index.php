582425020d7a472b29ed66088541281313772ec1
mashaelym <ma735@njit.edu>
2017-09-25 17:15:27 -0400

    fifth string function

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ $text = "String Functions";
 $obj->StringFunc($text);
 $obj->chr($value);
 $obj->strlen($name);
-$obj->rtrim(Sstr);
+$obj->rtrim($str);
+$obj->htmlentities($val);
+$obj->strpos($strr,$strr2);
 class main {
 public function __construct() {
 echo 'This is an object </br>';
@@ -14,27 +16,43 @@ public function StringFunc(){
 echo '<h1>String Functions:</h1>';
 print($text);
 }
-
+// first function
 public function chr($value) {
 echo '<h2>1: Chr() function</h2>';
 echo 'The ASCII character for the number 65 is ' . chr(65);
 echo '<hr>';
 }
+//second
 public function strlen($name){
 $name='mashael';
 echo '<h2>2: Strlen() function</h2>';
 echo 'The string length for ' . $name . ' is ' . strlen($name);
 echo '<hr>';
 }
- 
+ // third
 public function rtrim($str) {
-$str='    Yousef   ';
+$str="      Yousef      ";
 echo '<h2>3: Rtrim() function</h2>';
-echo 'We are trimming the right white space and the result is: ' .  rtrim($str);
+echo 'The name after Right trimming is: ' .  rtrim($str);
+echo '<hr>';
+}
+//fourth
+public function htmlentities($val) {
+$val="&amp";
+echo '<h2>4: Htmlspecialchars() function</h2>';
+echo 'The HTML special character for ' . $val . ' is ' . htmlentities($val);
 echo '<hr>';
 }
 
+//fifth 
+public function strpos($strr,$strr2) {
+$strr= 'Mashael Almoqbel is a Student';
+$strr2= 'is';
+echo '<h2>5: Strpos() function</h2>';
+echo 'The position of the string '. '(' . $strr2  . ')' . ' in this sentence ' . '(' . $strr . ')' . ' is: '. $i=strpos($strr,$strr2);
+echo '<hr>';
 
+}
 
 public function __destruct() {
 echo '</br> Finished!';
