d58b38d056b7cca774e0d557eecdfde22d837964
mashaelym <ma735@njit.edu>
2017-09-25 11:41:44 -0400

    adding construct and deconstruct objects

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$prog =new main();
+class main {
+public function __construct() {
+echo 'This is an object </br>';
+}
+
+
+
+
+public function __destruct() {
+echo '</br> Finished!';
+}
+}
+
+
+?>
