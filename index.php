ba4e2f9dfa385fb03ca68117d1c2d660bb58b192
mashaelym <ma735@njit.edu>
2017-09-25 13:06:06 -0400

    adding first string function

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $obj =new main();
 $text = "String Functions";
 $obj->StringFunc($text);
+$obj->chr($value);
 
 class main {
 public function __construct() {
@@ -11,6 +12,11 @@ echo 'This is an object </br>';
 public function StringFunc(){
 echo '<h1>String Functions:</h1>';
 print($text);
+}
+
+public function chr($value) {
+echo '<h2>1: Chr() function</h2>';
+echo 'The ASCII character for the number 65 is ' . chr(65);
 echo '<hr>';
 }
 
